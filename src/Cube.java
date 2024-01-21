a4c2535c81680dd48f5d300f0490a5bda17e66f4
Eric <erict2026@student.cis.edu.hk>
2024-01-21 14:05:30 +0800

    done, run configuration bugged out though

---
+++ src/Cube.java
@@ -0,0 +1,36 @@
+public class Cube {
+    private int side;
+    public Cube(){
+
+        side = 1;
+
+    }
+    public Cube(int side){
+
+        if (side < 1){
+            throw new IllegalArgumentException("A cube’s side length cannot be less than 1");
+        }
+        setSide(side);
+    }
+    public int getSide(){
+        return side;
+    }
+    public void setSide(int side){
+        if (side < 1){
+            throw new IllegalArgumentException("A cube’s side length cannot be less than 1");
+        }
+        this.side = side;
+    }
+    public int getSurfaceArea(){
+        int face = side * side;
+        int surfaceArea = face * 6;
+        return surfaceArea;
+    }
+    public int getVolume(){
+        int volume = side * side * side;
+        return volume;
+    }
+    public String toString(){
+        return "Cube{side=" + side + "}";
+    }
+}
